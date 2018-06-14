2e2a213f8a3fb3d8f0890c1425958b9a7f7515cf
Jeff Kluge <jeffkl@microsoft.com>
2018-06-14 10:49:21 -0700

    Migrate projects
    * Back to stock, away from CBT
    * Nerdbank.GitVersioning
    * Central package versions
    * appveyor.yml

--- src/Microsoft.Dism/Properties/AssemblyInfo.cs
+++ src/Microsoft.Dism/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyTitle("Microsoft.Dism")]
-[assembly: AssemblyDescription("Managed API for Deployment Image Servicing and Management (DISM)")]
+//[assembly: AssemblyTitle("Microsoft.Dism")]
+//[assembly: AssemblyDescription("Managed API for Deployment Image Servicing and Management (DISM)")]
 
 [assembly: InternalsVisibleTo("Microsoft.Dism.Tests")]
