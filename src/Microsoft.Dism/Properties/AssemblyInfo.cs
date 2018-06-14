7937a02ecd308b9bcd1a8bea39c274d83460d9b7
Jeff Kluge <jeffkl@microsoft.com>
2018-06-14 10:52:15 -0700

    Update assembly info

--- src/Microsoft.Dism/Properties/AssemblyInfo.cs
+++ src/Microsoft.Dism/Properties/AssemblyInfo.cs
@@ -1,7 +1,3 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-
-//[assembly: AssemblyTitle("Microsoft.Dism")]
-//[assembly: AssemblyDescription("Managed API for Deployment Image Servicing and Management (DISM)")]
+﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Microsoft.Dism.Tests")]
