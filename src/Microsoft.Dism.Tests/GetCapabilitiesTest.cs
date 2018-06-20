0de9c22faf7b822c4ad87c9e984802e5b65e47c1
Jeff Kluge <jeffkl@microsoft.com>
2018-06-20 12:43:58 -0700

    Add GetCapabilitiesTest and GetImageInfoTest

---
+++ src/Microsoft.Dism.Tests/GetCapabilitiesTest.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c). All rights reserved.
+//
+// Licensed under the MIT license.
+
+using Shouldly;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.Dism.Tests
+{
+    public class GetCapabilitiesTest : DismTestBase
+    {
+        public GetCapabilitiesTest(TestWimTemplate template, ITestOutputHelper testOutput)
+            : base(template, testOutput)
+        {
+        }
+
+        [Fact]
+        public void GetCapabilitiesOnlineSession()
+        {
+            using (DismSession session = DismApi.OpenOnlineSession())
+            {
+                DismCapabilityCollection capabilities = DismApi.GetCapabilities(session);
+
+                capabilities.Count.ShouldBeGreaterThan(0);
+
+                foreach (DismCapability capability in capabilities)
+                {
+                    capability.Name.ShouldNotBeNullOrWhiteSpace();
+
+                    capability.State.ShouldBeOneOf(DismPackageFeatureState.Installed, DismPackageFeatureState.NotPresent);
+                }
+            }
+        }
+    }
+}
