e9651eff96dd368588e71661f3d42305ca069391
Jeff Kluge <jeffkl@microsoft.com>
2018-06-20 11:25:17 -0700

    Add CheckImageHealthTest
    * Add DismTestBase for unit tests that need DismApi to be initialized/shut down

---
+++ src/Microsoft.Dism.Tests/Base/DismTestBase.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c). All rights reserved.
+//
+// Licensed under the MIT license.
+
+using System.IO;
+using Xunit.Abstractions;
+
+namespace Microsoft.Dism.Tests
+{
+    public abstract class DismTestBase : TestBase
+    {
+        private readonly string _logFilePath;
+        private readonly ITestOutputHelper _testOutput;
+
+        protected DismTestBase(TestWimTemplate template, ITestOutputHelper testOutput)
+            : base(template)
+        {
+            _testOutput = testOutput;
+
+            _logFilePath = Path.Combine(TestDirectory, "dism.log");
+
+            DismApi.Initialize(DismLogLevel.LogErrorsWarningsInfo, _logFilePath);
+        }
+
+        public override void Dispose()
+        {
+            DismApi.Shutdown();
+
+            _testOutput.WriteLine(File.ReadAllText(_logFilePath));
+
+            base.Dispose();
+        }
+    }
+}
---
+++ src/Microsoft.Dism.Tests/CheckImageHealthTest.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (c). All rights reserved.
+//
+// Licensed under the MIT license.
+
+using Shouldly;
+using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.Dism.Tests
+{
+    public class CheckImageHealthTest : DismTestBase
+    {
+        public CheckImageHealthTest(TestWimTemplate template, ITestOutputHelper testOutput)
+            : base(template, testOutput)
+        {
+        }
+
+        [Fact]
+        public void CheckImageHealthOnlineSession()
+        {
+            using (DismSession session = DismApi.OpenOnlineSession())
+            {
+                DismImageHealthState imageHealthState = DismApi.CheckImageHealth(session, scanImage: false, progressCallback: null, userData: null);
+
+                imageHealthState.ShouldBe(DismImageHealthState.Healthy);
+            }
+        }
+
+        [Fact]
+        public void CheckImageHealthOnlineSessionWithCallback()
+        {
+            const string expectedUserData = "257E41FC307248608E28C3B1F8D5CF09";
+
+            string userData = null;
+
+            int current = -1;
+            int total = -1;
+
+            using (DismSession session = DismApi.OpenOnlineSession())
+            {
+                try
+                {
+                    DismApi.CheckImageHealth(
+                        session: session,
+                        scanImage: true, // Setting scanImage to true seems to trigger the callback being called
+                        progressCallback: progress =>
+                        {
+                            userData = progress.UserData as string;
+                            current = progress.Current;
+                            total = progress.Total;
+
+                            // Cancel the operation, otherwise it takes ~1 min
+                            progress.Cancel = true;
+                        },
+                        userData: expectedUserData);
+                }
+                catch (OperationCanceledException)
+                {
+                }
+
+                userData.ShouldBe(expectedUserData);
+                current.ShouldBe(50);
+                total.ShouldBe(1000);
+            }
+        }
+    }
+}
