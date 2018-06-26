11ed848a0fa4d9ff44791c5dff9e3ad80881eb84
Jeff Kluge <jeffkl@microsoft.com>
2018-06-26 08:39:07 -0700

    Add missing Regions field to DismAppxPackage_ (#59)
    * Add missing Regions field to DismAppxPackage_
    
    This was causing fatal errors when marshalling the struct because the size was off.  Also added a unit test.
    
    Fixes #58
    
    * Update test because AppVeyor machine has no packages installed :(

---
+++ src/Microsoft.Dism.Tests/GetProvisionedAppxPackagesTest.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c). All rights reserved.
+//
+// Licensed under the MIT license.
+
+using Shouldly;
+using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.Dism.Tests
+{
+    public class GetProvisionedAppxPackagesTest : DismTestBase
+    {
+        public GetProvisionedAppxPackagesTest(TestWimTemplate template, ITestOutputHelper testOutput)
+            : base(template, testOutput)
+        {
+        }
+
+        [Fact]
+        public void GetProvisionedAppxPackagesSimple()
+        {
+            using (DismSession onlineSession = DismApi.OpenOnlineSession())
+            {
+                DismAppxPackageCollection packages = DismApi.GetProvisionedAppxPackages(onlineSession);
+
+                packages.ShouldNotBeNull();
+
+                foreach (DismAppxPackage package in packages)
+                {
+                    package.Architecture.ShouldNotBe(DismProcessorArchitecture.None);
+                    package.DisplayName.ShouldNotBeNullOrWhiteSpace();
+                    package.InstallLocation.ShouldNotBeNullOrWhiteSpace();
+                    package.PackageName.ShouldNotBeNullOrWhiteSpace();
+                    package.PublisherId.ShouldNotBeNullOrWhiteSpace();
+                    package.ResourceId.ShouldNotBeNullOrWhiteSpace();
+                    package.Version.ShouldBeGreaterThan(Version.Parse("0.0.0.0"));
+                }
+            }
+        }
+    }
+}
