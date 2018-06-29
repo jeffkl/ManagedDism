d12e49dbbcf29d216db6f02b0befe3dda3c4019a
Jeff Kluge <jeffkl@microsoft.com>
2018-06-29 09:24:06 -0700

    Add GetPackagesTest

---
+++ src/Microsoft.Dism.Tests/GetPackagesTest.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c). All rights reserved.
+//
+// Licensed under the MIT license.
+
+using Shouldly;
+using System;
+using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.Dism.Tests
+{
+    public class GetPackagesTest : DismTestBase
+    {
+        public GetPackagesTest(TestWimTemplate template, ITestOutputHelper testOutput)
+            : base(template, testOutput)
+        {
+        }
+
+        [Fact]
+        public void GetPackagesSimple()
+        {
+            using (DismSession session = DismApi.OpenOnlineSession())
+            {
+                DismPackageCollection packages = DismApi.GetPackages(session);
+
+                packages.ShouldNotBeNull();
+
+                foreach (DismPackage package in packages)
+                {
+                    package.InstallTime.ShouldBeGreaterThan(DateTime.MinValue);
+                    package.PackageName.ShouldNotBeNullOrWhiteSpace();
+                    package.PackageState.ShouldBeOneOf(Enum.GetValues(typeof(DismPackageFeatureState)).Cast<DismPackageFeatureState>().ToArray());
+                    package.ReleaseType.ShouldBeOneOf(Enum.GetValues(typeof(DismReleaseType)).Cast<DismReleaseType>().ToArray());
+                }
+            }
+        }
+    }
+}
