a5f61b5df3907805287c9b91ccc8c14fc9cb800c
Jeff Kluge <jeffkl@microsoft.com>
2018-07-02 11:18:14 -0700

    Add WIM that can be used for functional tests (#65)
    install.wim is the smallest WIM I could find that tests can run against
    
    Addresss #32

---
+++ src/Microsoft.Dism.Tests/ApplyUnattendTest.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c). All rights reserved.
+//
+// Licensed under the MIT license.
+
+using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.Dism.Tests
+{
+    public class ApplyUnattendTest : DismInstallWimTestBase
+    {
+        public ApplyUnattendTest(TestWimTemplate template, ITestOutputHelper testOutput)
+            : base(template, testOutput)
+        {
+        }
+
+        [Fact]
+        public void ApplyUnattendSimple()
+        {
+            const string unattendXml = @"<?xml version=""1.0"" encoding=""utf-8""?>
+<unattend xmlns=""urn:schemas-microsoft-com:unattend"">
+<settings pass=""windowsPE"">
+        <component name=""Microsoft-Windows-Setup"" processorArchitecture=""x86"" publicKeyToken=""31bf3856ad364e35"" language=""neutral"" versionScope=""nonSxS"" xmlns:wcm=""http://schemas.microsoft.com/WMIConfig/2002/State"" xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"">
+            <UserData>
+                <ProductKey>
+                    <WillShowUI>Never</WillShowUI>
+                    <Key>XXXX-XXXX-XXXX-XXXX-XXXX</Key>
+                </ProductKey>
+                <FullName>Full Name</FullName>
+                <Organization>Organization Name</Organization>
+                <AcceptEula>true</AcceptEula>
+            </UserData>
+        </component>
+    </settings>
+                     </unattend>";
+
+            string unattendXmlFile = Path.Combine(TestDirectory, "unattend.xml");
+
+            File.WriteAllText(unattendXmlFile, unattendXml);
+
+            DismApi.ApplyUnattend(Session, unattendXmlFile, singleSession: true);
+        }
+    }
+}
---
+++ src/Microsoft.Dism.Tests/Base/DismInstallWimTestBase.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c). All rights reserved.
+//
+// Licensed under the MIT license.
+
+using System;
+using Xunit.Abstractions;
+
+namespace Microsoft.Dism.Tests
+{
+    public abstract class DismInstallWimTestBase : DismTestBase
+    {
+        private readonly Lazy<DismSession> _session;
+
+        protected DismInstallWimTestBase(TestWimTemplate template, ITestOutputHelper testOutput)
+            : base(template, testOutput)
+        {
+            _session = new Lazy<DismSession>(GetSession, isThreadSafe: true);
+        }
+
+        protected DismSession Session => _session.Value;
+
+        public override void Dispose()
+        {
+            if (_session.IsValueCreated)
+            {
+                _session.Value.Dispose();
+
+                DismApi.UnmountImage(MountPath, commitChanges: false);
+            }
+
+            base.Dispose();
+        }
+
+        private DismSession GetSession()
+        {
+            foreach (DismMountedImageInfo mountedImageInfo in DismApi.GetMountedImages())
+            {
+                DismApi.UnmountImage(mountedImageInfo.MountPath, false);
+            }
+
+            DismApi.CleanupMountpoints();
+
+            DismApi.MountImage(InstallWimPath, MountPath, 1);
+
+            return DismApi.OpenOfflineSession(MountPath);
+        }
+    }
+}
