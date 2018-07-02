160f2d21ae8f96b960224a387ac873c34a10811a
Jeff Kluge <jeffkl@microsoft.com>
2018-07-02 11:07:38 -0700

    Add WIM that can be used for functional tests
    install.wim is the smallest WIM I could find that tests can run against
    
    Addresss #32

--- src/Microsoft.Dism.Tests/Base/TestBase.cs
+++ src/Microsoft.Dism.Tests/Base/TestBase.cs
@@ -4,6 +4,7 @@
 
 using System;
 using System.IO;
+using System.Reflection;
 using Xunit;
 
 namespace Microsoft.Dism.Tests
@@ -17,7 +18,19 @@ namespace Microsoft.Dism.Tests
         protected TestBase(TestWimTemplate template)
         {
             Template = template;
+
+            MountPath = Directory.CreateDirectory(Path.Combine(TestDirectory, "mount")).FullName;
+
+            TestAssemblyDirectory = Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath);
+
+            InstallWimPath = Path.Combine(TestAssemblyDirectory, "install.wim");
         }
+
+        public string InstallWimPath { get; set; }
+
+        public string MountPath { get; set; }
+
+        public string TestAssemblyDirectory { get; set; }
 
         protected TestWimTemplate Template { get; }
 
@@ -25,6 +38,7 @@ namespace Microsoft.Dism.Tests
 
         public virtual void Dispose()
         {
+            Directory.Delete(TestDirectory, recursive: true);
         }
     }
 }
