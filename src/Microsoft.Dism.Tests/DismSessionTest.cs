1277c525c7868fe21b05d437184312fb2711f27a
Jack Hennessey <jhennessey5225@gmail.com>
2021-03-01 16:41:19 -0500

    Added the ability to suppress exceptions when a reboot is required (#110)
    Fixes #109

---
+++ src/Microsoft.Dism.Tests/DismSessionTest.cs
@@ -0,0 +1,105 @@
+﻿// Copyright (c). All rights reserved.
+//
+// Licensed under the MIT license.
+
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.Dism.Tests
+{
+    public class DismSessionTest : DismTestBase
+    {
+        public DismSessionTest(TestWimTemplate template, ITestOutputHelper testOutput)
+            : base(template, testOutput)
+        {
+        }
+
+        [Fact]
+        public void SessionOptionsDefaults()
+        {
+            DismSessionOptions options = new DismSessionOptions();
+
+            options.ThrowExceptionOnRebootRequired.ShouldBeTrue();
+        }
+
+        [Theory]
+        [ClassData(typeof(SessionOptionsBehaviorData))]
+        public void SessionOptionsBehavior(DismSessionOptions options, Func<DismSessionOptions, DismSession> sessionFunc)
+        {
+            using (DismSession session = sessionFunc(options))
+            {
+                session.Options.ShouldNotBeNull();
+                session.RebootRequired.ShouldBeFalse();
+
+                if (options == null)
+                {
+                    session.Options.ThrowExceptionOnRebootRequired.ShouldBeTrue();
+                }
+                else
+                {
+                    session.Options.ShouldBe(options);
+                }
+
+                DismUtilities.ThrowIfFail(DismApi.ERROR_SUCCESS, session);
+                session.RebootRequired.ShouldBeFalse();
+
+                if (session.Options.ThrowExceptionOnRebootRequired)
+                {
+                    Should.Throw<DismRebootRequiredException>(() => DismUtilities.ThrowIfFail(DismApi.ERROR_SUCCESS_REBOOT_REQUIRED, session));
+                }
+                else
+                {
+                    DismUtilities.ThrowIfFail(DismApi.ERROR_SUCCESS_REBOOT_REQUIRED, session);
+                }
+
+                session.RebootRequired.ShouldBeTrue();
+
+                session.RebootRequired = false;
+                session.RebootRequired.ShouldBeTrue();
+            }
+        }
+
+        private class SessionOptionsBehaviorData : TheoryData<DismSessionOptions, Func<DismSessionOptions, DismSession>>
+        {
+            private readonly List<DismSessionOptions> _sessionOptions = new List<DismSessionOptions>
+            {
+                null,
+                new DismSessionOptions(),
+                new DismSessionOptions { ThrowExceptionOnRebootRequired = false },
+            };
+
+            private readonly List<Func<DismSessionOptions, DismSession>> _publicSessionMethods = new List<Func<DismSessionOptions, DismSession>>
+            {
+                (_) => DismApi.OpenOnlineSession(),
+                (_) => DismApi.OpenOfflineSession(DismApi.DISM_ONLINE_IMAGE),
+                (_) => DismApi.OpenOfflineSession(DismApi.DISM_ONLINE_IMAGE, null, null),
+            };
+
+            private readonly List<Func<DismSessionOptions, DismSession>> _publicSessionExMethods = new List<Func<DismSessionOptions, DismSession>>
+            {
+                (options) => DismApi.OpenOnlineSessionEx(options),
+                (options) => DismApi.OpenOfflineSessionEx(DismApi.DISM_ONLINE_IMAGE, options),
+                (options) => DismApi.OpenOfflineSessionEx(DismApi.DISM_ONLINE_IMAGE, null, null, options),
+            };
+
+            public SessionOptionsBehaviorData()
+            {
+                foreach (var sessionMethod in _publicSessionMethods)
+                {
+                    Add(null, sessionMethod);
+                }
+
+                foreach (DismSessionOptions options in _sessionOptions)
+                {
+                    foreach (var sessionMethodEx in _publicSessionExMethods)
+                    {
+                        Add(options, sessionMethodEx);
+                    }
+                }
+            }
+        }
+    }
+}
--- src/Microsoft.Dism/DismSession.cs
+++ src/Microsoft.Dism/DismSession.cs
@@ -16,6 +16,7 @@ namespace Microsoft.Dism
         private readonly string _imagePath;
         private readonly string _systemDrive;
         private readonly string _windowsDirectory;
+        private bool _rebootRequired;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="DismSession" /> class.
@@ -23,15 +24,35 @@ namespace Microsoft.Dism
         /// <param name="imagePath">An absolute or relative path to the root directory of an offline Windows image, an absolute or relative path to the root directory of a mounted Windows image, or DISM_ONLINE_IMAGE to associate with the online Windows installation.</param>
         /// <param name="windowsDirectory">A relative or absolute path to the Windows directory. The path is relative to the mount point.</param>
         /// <param name="systemDrive">The letter of the system drive that contains the boot manager. If SystemDrive is NULL, the default value of the drive containing the mount point is used.</param>
-        internal DismSession(string imagePath, string windowsDirectory, string systemDrive)
+        /// <param name="options">A <see cref="DismSessionOptions"/> object that contains the options for the session.</param>
+        internal DismSession(string imagePath, string windowsDirectory, string systemDrive, DismSessionOptions options = null)
             : base(true)
         {
             _imagePath = imagePath;
             _windowsDirectory = windowsDirectory;
             _systemDrive = systemDrive;
 
+            Options = options ?? new DismSessionOptions();
+
             Reload();
         }
+
+        /// <summary>
+        /// Gets a value indicating whether or not a reboot is required.
+        /// </summary>
+        public bool RebootRequired
+        {
+            get => _rebootRequired;
+            internal set
+            {
+                _rebootRequired = _rebootRequired || value;
+            }
+        }
+
+        /// <summary>
+        /// Gets the options for the session.
+        /// </summary>
+        internal DismSessionOptions Options { get; }
 
         /// <summary>
         /// Reloads the session by closing the current session and opening it again.
