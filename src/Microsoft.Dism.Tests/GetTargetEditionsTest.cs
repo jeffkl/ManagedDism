ce283352fb9d346d7036c4863e2a4cca01995dde
hdensity <sam@schm.it>
2024-12-05 08:44:37 +0100

    Added GetTargetEditions

---
+++ src/Microsoft.Dism.Tests/GetTargetEditionsTest.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c). All rights reserved.
+//
+// Licensed under the MIT license.
+
+using Shouldly;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.Dism.Tests
+{
+    public class GetTargetEditionsTest : DismTestBase
+    {
+        public GetTargetEditionsTest(TestWimTemplate template, ITestOutputHelper testOutput)
+            : base(template, testOutput)
+        {
+        }
+
+        [Fact]
+        public void GetTargetEditionsOnlineSession()
+        {
+            using (DismSession session = DismApi.OpenOnlineSession())
+            {
+                DismEditionCollection editionCollection = DismApi.GetTargetEditions(session);
+
+                editionCollection.ShouldNotBeNull();
+                editionCollection.ShouldNotBeEmpty();
+            }
+        }
+    }
+}
