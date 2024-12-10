c97093bd56b2b0f13284b1c6537a79152fdb1e53
Sam Schmit-Van Werweke <sam@schm.it>
2024-12-10 19:44:17 +0100

    Add GetRegistryMountPoint (#248)

---
+++ src/Microsoft.Dism.Tests/GetRegistryMountPointTest.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c). All rights reserved.
+//
+// Licensed under the MIT license.
+
+using Shouldly;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.Dism.Tests
+{
+    public class GetRegistryMountPointTest : DismInstallWimTestBase
+    {
+        public GetRegistryMountPointTest(TestWimTemplate template, ITestOutputHelper testOutput)
+            : base(template, testOutput)
+        {
+        }
+
+        [Theory]
+        [InlineData(DismRegistryHive.Software)]
+        [InlineData(DismRegistryHive.System)]
+        [InlineData(DismRegistryHive.Security)]
+        [InlineData(DismRegistryHive.SAM)]
+        [InlineData(DismRegistryHive.Default)]
+        [InlineData(DismRegistryHive.HKCU)]
+        [InlineData(DismRegistryHive.Components)]
+        [InlineData(DismRegistryHive.Drivers)]
+        public void GetRegistryMountPointOfflineImage(DismRegistryHive dismRegistryHive)
+        {
+            string value = DismApi.GetRegistryMountPoint(Session, dismRegistryHive);
+
+            value.ShouldNotBeNullOrWhiteSpace();
+        }
+    }
+}
